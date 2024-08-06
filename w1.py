26be0cd53852c3c44c85f2a1465fc3818a715c36
sebygaa <63217093+sebygaa@users.noreply.github.com>
2024-08-06 16:21:18 +0900

    Create w1.py

---
+++ w1.py
@@ -0,0 +1,28 @@
+a = 1
+b = 2
+e = a+b
+
+A = 3
+B = 4
+E = A + B
+
+a_str = 'a'
+b_str = 'b'
+e_str = a_str + b_str
+
+print('e = ')
+print(e)
+print()
+
+print('E = ')
+print(E)
+print()
+
+print('e_str = ')
+print(e_str)
+
+print('type(a) =')
+print(type(a))
+
+print('type(a_str) = ')
+print(type(a_str))
